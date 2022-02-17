b94c67b23157085a1b9e9ee787d877ab55db6237
German Goncharenko <gonharenkogerman@gmail.com>
2022-02-17 19:50:39 +0300

    Задания к 4 семинару

---
+++ Lession_04/Task_26/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = new Random().Next(1, 10);
+int exponent = new Random().Next(1, 10);
+
+int GetExponentiation(int number, int exponent)
+{
+    int count = 1;
+    int result = number;
+    while (count < exponent)
+    {
+        result = result * number;
+        count++;
+    }
+    return result;
+}
+
+int result = GetExponentiation(number, exponent);
+Console.WriteLine($"{number} ^ {exponent} = {result}");
---
+++ Lession_04/Task_30/Program.cs
@@ -0,0 +1,12 @@
+﻿int N = new Random().Next(1, 30);
+Console.WriteLine($"N = {N}");
+
+int number = 1;
+Console.WriteLine("By While");
+while (number <= N)
+{
+    int cube = number * number * number;
+    if (cube % 2 == 0) Console.Write(cube + " ");
+    number++;
+}
+Console.WriteLine();
