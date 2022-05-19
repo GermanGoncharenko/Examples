d69e0e746e2bff15c62c2f150ac493032ff77da0
goncharenko <German.Goncharenko@mvideo.ru>
2022-05-19 19:51:49 +0300

    add homework_5

---
+++ Lession_09/Task_67/Program.cs
@@ -0,0 +1,21 @@
+﻿class RecExercise1
+{
+    static int printNatural(int ctr,int stval)
+    {
+	if (ctr < 1)
+	{
+	    return stval;
+	}
+	Console.Write(" {0} ",ctr);
+		ctr--;
+	return printNatural(ctr,stval);
+    }
+    
+    static void Main()
+    {
+	Console.Write("Введите число: ");
+	int ctr= Convert.ToInt32(Console.ReadLine());	
+	printNatural(ctr,1);
+	Console.Write("\n\n");
+    }
+}
